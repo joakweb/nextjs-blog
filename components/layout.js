d25b54565be945e0819b6ba0c802a8a1656e18f9
Jorge Rodrigues Carvalho <jorgecarvalho@lampp-it.com.br>
2023-05-15 16:31:31 -0300

    feat: dynamic routes and utility styling

--- components/layout.js
+++ components/layout.js
@@ -4,7 +4,7 @@ import styles from "./layout.module.css";
 import utilStyles from "../styles/utils.module.css";
 import Link from "next/link";
 
-const name = "Jorge R. Carvalho";
+const name = "JRC";
 export const siteTitle = "Next.js CV";
 
 export default function Layout({ children, home }) {
